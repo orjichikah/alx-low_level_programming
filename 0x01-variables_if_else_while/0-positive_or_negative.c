b199229feb65bb063fe49ad470e29e8786df9f29
Chika Orji <orjichikahappiness@yahoo.com>
2023-03-20 19:12:01 -0700

    Code for printing int type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%d is positive", n);
 	else if (n == 0)
-		printf("%d is equal to zero\n", n);
+		printf("%d is equal to zero", n);
 	else
 		printf("%d is negative", n);
+	printf("\n");
 	return (0);
 }
