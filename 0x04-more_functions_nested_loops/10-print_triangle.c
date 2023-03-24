b813e36fb873e56dc8ba15ee775f6dc43397ece7
Chika Orji <orjichikahappiness@yahoo.com>
2023-03-23 19:03:45 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: the size of the triangle.
+ *
+ * Return: void.
+ */
+
+void print_triangle(int size)
+
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - a program that finds and prints the largest 
+ * prime factor of the number 612852475143.
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int num;
+}
